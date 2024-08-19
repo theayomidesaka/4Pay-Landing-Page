ef5e7d4d5b6bb68ac5c5e1b2bb1b9f4a52ba4cc3
Ayomide Saka <ayomidesaka@MacBook-Pro.local>
2024-08-19 08:29:42 +0100

    Added an FAQ section and made it mobile responsive

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("My name")
+const menuIcon = document.getElementById("menu-icon")
+const closeIcon = document.getElementById("close-icon")
+const mobileMenu = document.getElementById("mobile-menu")
+
+
+menuIcon.addEventListener("click", () => {
+    mobileMenu.style.display = "flex"
+})
+
+closeIcon.addEventListener("click", function(){
+    mobileMenu.style.display = "none"
+})
+
+
+
+
+const faqs = document.querySelectorAll(".FAQ-1, .FAQ-7");
+
+faqs.forEach((faq) =>{
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    }) ;
+});
